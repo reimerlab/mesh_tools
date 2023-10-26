f3c1e9669e20413eb932f34c98876797498b1147
root <bac8@rice.edu>
2023-10-26 18:37:57 +0000

    no long description in setup.py

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # mesh_tools
+
+Utility functions for mesh representation objects (like trimesh) and manipulations using 3rd party packages (CGAL, Meshlab, etc.). All system requirements for 3rd party packages and python wrappers are performed in Dockerfile
+
+## Setup: Installation inside docker env
+
+---
+
+### Download Docker Image
+
+```bash
+docker pull celiib/mesh_tools:v3
+```
+
+### Run Docker Container (from CLI)
+
+```bash
+mkdir notebooks
+
+docker container run -it \
+    -p 8890:8888 \
+    -v ./notebooks:/notebooks \
+    celiib/mesh_tools:v3
+```
--- setup.py
+++ setup.py
@@ -20,14 +20,29 @@ def get_links():
         #"datasci_tools @ git+https://github.com/bacelii/datasci_tools.git'"
     ]
 
+def get_long_description(filepath='README.md'):
+    try:
+        import pypandoc
+        long_description = pypandoc.convert_file(filepath, 'rst') 
+    except:
+        print("\n\n\n****Need to install pypandoc (and if havent done so install apt-get install pandoc) to make long description clean****\n\n\n")
+        
+        long_description = Path("README.md").read_text()
+        
+    return long_description
 
 
 setup(
-    name='mesh_tools', # the name of the package, which can be different than the folder when using pip instal
+    name='mesh_processing_tools', # the name of the package, which can be different than the folder when using pip instal
     version='1.0.0',
-    description='',
-    author='Brendan Celii',
-    author_email='brendanacelii',
+    description='Utility functions for mesh representation objects (like trimesh) and manipulations using 3rd party packages (CGAL, Meshlab, etc.)',
+    #long_description=get_long_description(),
+	project_urls={
+	    'Source':"https://github.com/reimerlab/mesh_tools",
+	    'Documentation':"https://reimerlab.github.io/mesh_tools/",
+	},
+	author='Brendan Celii',
+	author_email='brendanacelii@gmail.com',
     packages=find_packages(),  #teslls what packages to be included for the install
     install_requires=get_install_requires(), #external packages as dependencies
     # dependency_links = get_links(),
